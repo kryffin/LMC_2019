67e26539befb6fbc71acb9937f9df5ac59f906a3
nicolasklz <nicolaskleinhentz@gmail.com>
2019-11-19 19:45:07 +0100

    ajout des prédicats clash, decomposition, orient et check

--- unification.pl
+++ unification.pl
@@ -12,12 +12,14 @@ echo(T) :-
 echo(_).
 
 %
-% definition de l operateur ?=
+% definition de l operateur ?= qui prend deux arguments de la forme x ?= y avec une priorité de 20
 
 :- op(20, xfy, ?=).
 
 %
 % regle(E, R) qui indique si R est appliquable sur E
+%%% E : equation sur laquelle appliquer la règle
+%%% R : règle à appliquer
 
 regle(E, R) :-
 	arg(1, E, ArgGauche),
@@ -26,6 +28,8 @@ regle(E, R) :-
 
 %
 % occur check d une variable X sur un terme T
+%%% X : variable à rechercher
+%%% T : terme dans lequel rechercher
 
 occur_check(X, X) :-
 	!.
@@ -38,13 +42,47 @@ occur_check(V, T) :-
 	!.
 
 %
-% test et decomposition sur deux arguments (de la forme f(s, s, s), f(t, t, t))
-%%% WIP
+% test d un clash
+%%% ArgGauche : vecteur de gauche
+%%% ArgDroite : vecteur de droite
 
-decompose(ArgGauche, ArgDroite) :-
+clash(ArgGauche, ArgDroite) :-
 	functor(ArgGauche, F, AriteGauche),
 	functor(ArgDroite, F, AriteDroite),
-	AriteGauche == AriteDroite,
-	arg(ElemI, ArgGauche, Si),
-	arg(ElemI, ArgDroite, Ti).
-	%ajouter[?=(Si, Ti)] a Q
+	AriteGauche == AriteDroite.
+
+%
+% application de la regle de décomposition
+%%% S : vecteur de gauche
+%%% T : vecteur de droite
+%%% Q : résultat (par unification)
+%%% N : indice
+
+decompose(S, T, Q, 1) :- !.
+
+decompose(S, T, Q, N) :-
+	succ(M, N),
+	arg(M, S, Si),
+	arg(M, T, Ti),
+	L = [?=(Si, Ti)|Q],
+	decompose(S, T, L, M).
+
+%
+% reoriente une equation t ?= x en x ?= t si t est une variable
+%%% E : equation a reorienter
+
+orient(S, T) :-
+    atom(T),
+    write(T ?= S).
+
+%
+% check appelle un occur_check sur S et T
+%%% S : vecteur gauche
+%%% T : vecteur droite
+
+check(S, T) :-
+    occur_check(S, T).
+
+%
+% simplify
+%%% WIP
