ff24f7dfc6d8ce7edfefb64c1623d380639cfa86
nicolasklz <nicolaskleinhentz@gmail.com>
2019-11-24 19:14:05 +0100

    ajout de rename et simplify + utilitaires sur liste

--- unification.pl
+++ unification.pl
@@ -1,5 +1,6 @@
 %
-% primitives d affichage
+%%
+%%% primitives d affichage
 
 set_echo :-
 	assert(echo_on).
@@ -15,6 +16,44 @@ echo(_).
 % definition de l operateur ?= qui prend deux arguments de la forme x ?= y avec une priorité de 20
 
 :- op(20, xfy, ?=).
+
+*
+**
+*** utilitaires
+
+%
+% reverse retourne une liste
+%%% [H|T] : liste à retourner
+%%% Z : liste retournée
+%%% Acc : liste intermédiaire
+
+reverse([],Z,Z).
+
+reverse([H|T],Z,Acc) :-
+    reverse(T,Z,[H|Acc]).
+
+%
+% suppr supprime un element E de la liste Q
+%%% E : element à supprimer
+%%% [H|Q] : liste d entrée
+%%% L : liste de sortie
+
+suppr(E, [], L) :-
+    reverse(L, Z, []),
+    write(L).
+
+suppr(E, [H|Q], L) :-
+    E \= H,
+    suppr(E, Q, [H|L]),
+    !.
+
+suppr(E, [H|Q], L) :-
+    E == H,
+    suppr(E, Q, L).
+
+%
+%%
+%%% règles logiques
 
 %
 % regle(E, R) qui indique si R est appliquable sur E
@@ -42,14 +81,90 @@ occur_check(V, T) :-
 	!.
 
 %
-% test d un clash
-%%% ArgGauche : vecteur de gauche
-%%% ArgDroite : vecteur de droite
+%%
+%%% règles Martelli-Montanari
 
-clash(ArgGauche, ArgDroite) :-
-	functor(ArgGauche, F, AriteGauche),
-	functor(ArgDroite, F, AriteDroite),
-	AriteGauche == AriteDroite.
+%
+% rename vérifie si S est renommable par T (ssi T est une variable)
+%%% S : vecteur gauche
+%%% T : vecteur droit
+
+rename(S, T) :-
+    var(T).
+
+%
+% rename qui remplace les t variables par x
+%%% E : element de remplacement
+%%% [H|Q] : liste sur laquelle remplacer
+%%% L : liste du résultat
+
+rename(S, [], L) :- write(L).
+
+rename(E, [H|Q], L) :-
+    arg(1, E, ArgUn),
+    arg(2, E, ArgDeux),
+    arg(1, H, S),
+    arg(2, H, T),
+    T == ArgUn,
+    rename(E, Q, [?=(S, ArgDeux)|L]).
+
+rename(E, [H|Q], L) :-
+    arg(1, E, ArgUn),
+    arg(2, E, ArgDeux),
+    arg(1, H, S),
+    arg(2, H, T),
+    T \= ArgUn,
+    rename(E, Q, [?=(S, T)|L]),
+    !.
+
+%
+% simplify vérifie si S est renommable par T (ssi T est une constante)
+%%% S : vecteur gauche
+%%% T : vecteur droit
+
+simplify(S, T) :-
+    functor(T, T, 0).
+
+%
+% simplify qui remplace les t constantes par x
+%%% E : element de remplacement
+%%% [H|Q] : liste sur laquelle remplacer
+%%% L : liste du résultat
+
+simplify(S, [], L) :- write(L).
+
+simplify(E, [H|Q], L) :-
+    arg(1, E, ArgUn),
+    arg(2, E, ArgDeux),
+    arg(1, H, S),
+    arg(2, H, T),
+    T == ArgUn,
+    rename(E, Q, [?=(S, ArgDeux)|L]).
+
+simplify(E, [H|Q], L) :-
+    arg(1, E, ArgUn),
+    arg(2, E, ArgDeux),
+    arg(1, H, S),
+    arg(2, H, T),
+    T \= ArgUn,
+    rename(E, Q, [?=(S, T)|L]),
+    !.
+
+%
+% check appelle un occur_check sur S et T
+%%% S : vecteur gauche
+%%% T : vecteur droite
+
+check(S, T) :-
+    occur_check(S, T).
+
+%
+% reoriente une equation t ?= x en x ?= t si t est une variable
+%%% E : equation a reorienter
+
+orient(S, T) :-
+    var(T),
+    write(T ?= S).
 
 %
 % application de la regle de décomposition
@@ -68,21 +183,11 @@ decompose(S, T, Q, N) :-
 	decompose(S, T, L, M).
 
 %
-% reoriente une equation t ?= x en x ?= t si t est une variable
-%%% E : equation a reorienter
+% test d un clash
+%%% ArgGauche : vecteur de gauche
+%%% ArgDroite : vecteur de droite
 
-orient(S, T) :-
-    atom(T),
-    write(T ?= S).
-
-%
-% check appelle un occur_check sur S et T
-%%% S : vecteur gauche
-%%% T : vecteur droite
-
-check(S, T) :-
-    occur_check(S, T).
-
-%
-% simplify
-%%% WIP
+clash(ArgGauche, ArgDroite) :-
+	functor(ArgGauche, F, AriteGauche),
+	functor(ArgDroite, F, AriteDroite),
+	AriteGauche == AriteDroite.
